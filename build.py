df79ad738e0bfa9e17c9623d300a7c2fb0bedd24
Ralph-Wang <ralph.wang@logicmonitor.com>
2018-04-09 22:19:41 +0800

    commponent and security

--- build.py
+++ build.py
@@ -28,52 +28,79 @@ class YamlParser(Parser):
     def dump(self, dictionary):
         return yaml.dump(dictionary, default_flow_style=False)
 
+class SwaggerDictWrapper(object):
+    def __init__(self):
+        self._document = {
+                "openapi": "",
+                "info": "",
+                "servers": [],
+                "paths": {},
+                "components": {},
+                "security": []
+        }
+        self._all_keys = list(self._document.keys())
+
+    def keys(self):
+        for key in self._all_keys:
+            yield key
+
+    def put(self, key, obj):
+        origin_obj = self._document[key]
+
+        if isinstance(origin_obj, str):
+            self._document[key] = obj
+
+        if isinstance(origin_obj, list):
+            self._document[key].append(obj)
+
+        if isinstance(origin_obj, dict):
+            self._document[key].update(obj)
+
+    def remove(self, key):
+        self._document.pop(key)
+
+    @property
+    def document(self):
+        return self._document
+
+
 
 class SwaggerWriter(object):
 
     def __init__(self, root_dir, parser):
+        self._dict_wrapper = SwaggerDictWrapper()
         self._document = { "openapi": "3.0.0" }
         self._root_dir = root_dir
         self._parser = parser
 
-    def build_info(self):
-        for filename in os.listdir('%s/%s' % (self._root_dir, 'info')):
-            fullname = '%s/%s/%s' % (self._root_dir, 'info', filename)
+    def _build(self, key):
+        dirname = '%s/%s' % (self._root_dir, key)
+        if not os.path.exists(dirname):
+            self._dict_wrapper.remove(key)
+            return
+        for filename in os.listdir('%s/%s' % (self._root_dir, key)):
+            fullname = '%s/%s/%s' % (self._root_dir, key, filename)
             with open(fullname) as fobj:
-                self._document['info'] = self._parser.load(fobj.read())
+                self._dict_wrapper.put(key, self._parser.load(fobj.read()))
 
-    def build_servers(self):
-        self._document['servers'] = []
-        for filename in os.listdir('%s/%s' % (self._root_dir, 'servers')):
-            fullname = '%s/%s/%s' % (self._root_dir, 'servers', filename)
-            with open(fullname) as fobj:
-                self._document['servers'].append(self._parser.load(fobj.read()))
-
-    def build_paths(self):
-        self._document['paths'] = {}
-        for filename in os.listdir('%s/%s' % (self._root_dir, 'paths')):
-            fullname = '%s/%s/%s' % (self._root_dir, 'paths', filename)
-            with open(fullname) as fobj:
-                self._document['paths'].update(self._parser.load(fobj.read()))
+    def build_all(self):
+        for key in self._dict_wrapper.keys():
+            self._build(key)
 
     def dumps(self):
-        print(self._parser.dump(self._document))
+        print(self._parser.dump(self._dict_wrapper.document))
 
 class CLI(object):
     def yaml(self, root_dir):
         writer = SwaggerWriter(root_dir, YamlParser())
 
-        writer.build_info()
-        writer.build_servers()
-        writer.build_paths()
+        writer.build_all()
         writer.dumps()
 
     def json(self, root_dir):
         writer = SwaggerWriter(root_dir, JSONParser())
 
-        writer.build_info()
-        writer.build_servers()
-        writer.build_paths()
+        writer.build_all()
         writer.dumps()
 
 def main():
